0698eeef7aa0bb3c983a3d4d76d6c60fdd43090a
BesovMusic <besovmusic@yandex.com>
2021-11-18 16:06:22 +0500

    Реализовал сравнение нажатой клавиши с символом Отображается текущий символ, реализована реакция на ввод неверных символов

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="text">
-            <span class="text__item" v-for="(symbol, index) in splitedText" :key="index">{{ symbol }}</span>
+            <span class="text__item" v-for="(symbol, index) in splitedText" :key="index" :class="{ current: currentSymbol == index }">{{ symbol }}</span>
         </div>
     </div>
 </template>
@@ -9,15 +9,34 @@
 <script>
 export default {
     name: "App",
-    components: {},
+    data() {
+        return {
+            currentSymbol: 0,
+        };
+    },
     computed: {
         splitedText() {
             let splitedText = this.$store.state.text.split("");
             return splitedText;
         },
     },
+    methods: {
+        buttonPressed(e) {
+            let currentSpan = document.querySelectorAll(".text__item");
+            if (currentSpan[this.currentSymbol].innerText == "") {
+                this.currentSymbol++;
+            } else {
+                if (e.key == currentSpan[this.currentSymbol].innerText) {
+                    this.currentSymbol++;
+                } else {
+                    currentSpan[this.currentSymbol].classList.add("wrong");
+                }
+            }
+        },
+    },
     mounted() {
         this.$store.dispatch("GET_TEXT_FROM_API");
+        document.addEventListener("keyup", this.buttonPressed);
     },
 };
 </script>
@@ -34,5 +53,16 @@ export default {
 .text {
     font-size: 18px;
     font-family: monospace;
+}
+.current {
+    background: green;
+    color: white;
+}
+.wrong {
+    background: red;
+    color: white;
+}
+.success {
+    color: green;
 }
 </style>
