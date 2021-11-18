13a92138cbde2bcefac1854f5330caccf22fbac3
BesovMusic <besovmusic@yandex.com>
2021-11-18 15:49:01 +0500

    Отрисовал текст на странице Полученный по API текст разделен на символы. Каждый символ отрисован в собственном элементе Span.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
-<template></template>
+<template>
+    <div class="container">
+        <div class="text">
+            <span class="text__item" v-for="(symbol, index) in splitedText" :key="index">{{ symbol }}</span>
+        </div>
+    </div>
+</template>
 
 <script>
 export default {
     name: "App",
     components: {},
+    computed: {
+        splitedText() {
+            let splitedText = this.$store.state.text.split("");
+            return splitedText;
+        },
+    },
     mounted() {
         this.$store.dispatch("GET_TEXT_FROM_API");
     },
@@ -18,5 +30,9 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+}
+.text {
+    font-size: 18px;
+    font-family: monospace;
 }
 </style>
